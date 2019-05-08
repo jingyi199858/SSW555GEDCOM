e453d76c8d355bc4c36e69fc827825a65ebd04a6
JingYi <jingdakai@gmail.com>
2019-05-08 14:40:08 -0400

    Update: Bug fixing: BirthBeforeMarriage.
    Test case update to help modify BirthBeforeMarriage.

--- src/main/java/org/Gedcom/BirthBeforeMarriage.java
+++ src/main/java/org/Gedcom/BirthBeforeMarriage.java
@@ -1,9 +1,18 @@
 package org.Gedcom;
 
+import org.gedcom4j.exception.GedcomParserException;
+import org.gedcom4j.model.Family;
+import org.gedcom4j.model.FamilyEvent;
+import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.enumerations.IndividualEventType;
+import org.gedcom4j.parser.GedcomParser;
+
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -11,81 +20,50 @@ import java.util.Scanner;
  * User story 2
  */
 public class BirthBeforeMarriage {
-    private static Scanner scan;
-    private static BufferedWriter writer;
 
     /**
-     * If found BIRTH before MARRY DATE, return thoes DATES as list. If not, return empty list.
+     * Birth should occur before marriage of an individual.
      * @param file
      * @return
      * @throws FileNotFoundException
      */
-    public static ArrayList birthMarri(String file) throws FileNotFoundException {
-        ArrayList invalid = new ArrayList();
-        ArrayList family = new ArrayList();
-        scan = new Scanner(new FileReader(file));
-        String reader = scan.nextLine();
-        String string = "";
-
-        while(scan.hasNextLine()){
-            string += reader;
-            reader = scan.nextLine();
-        }
-        int a = 0;
-        int b = 0;
-        String[] str = string.split("0 @F");
-        for(int i = 0; i < str.length; i++){
-            family.add(str[i]);
-        }
-
-        int fam[][] = new int[][]{{Integer.parseInt(family.get(1).toString().substring(20,21)),Integer.parseInt(family.get(1).toString().substring(34,35)),Integer.parseInt(family.get(1).toString().substring(61,65))},{Integer.parseInt(family.get(2).toString().substring(20,21)),Integer.parseInt(family.get(2).toString().substring(34,35)),Integer.parseInt(family.get(2).toString().substring(56,60))},{Integer.parseInt(family.get(3).toString().substring(19,21)),Integer.parseInt(family.get(3).toString().substring(33,35)),Integer.parseInt(family.get(3).toString().substring(49,53))},{Integer.parseInt(family.get(4).toString().substring(19,21)),Integer.parseInt(family.get(4).toString().substring(33,35)),Integer.parseInt(family.get(4).toString().substring(49,53))}};
-
-
-        String[] indi = family.get(0).toString().split("@ INDI");
-
-        ArrayList Birth = new ArrayList();
-        for(int i = 1; i < indi.length; i++){
-            a = indi[i].lastIndexOf("BIRT");
-            if(indi[i].lastIndexOf("BIRT") != -1) {
-                Birth.add(indi[i].substring(a + 11, a + 24));
-            }else{
-                Birth.add("No Data");
+    public static boolean birthMarri(String file) throws IOException, GedcomParserException {
+        GedcomParser gp = new GedcomParser();
+        gp.load(file);
+        Gedcom g = gp.getGedcom();
+        for (Family family : g.getFamilies().values()) {
+            List events = family.getEvents();
+            String husbandbirth = "";
+            String wifebirth = "";
+            if (events != null && events.size() > 0 && ((FamilyEvent) events.get(0)).getDate() != null) {
+                String marriage = ((FamilyEvent) events.get(0)).getDate().toString();
+                if (family.getHusband().getIndividual().getEventsOfType(IndividualEventType.BIRTH).size() > 0 && family.getHusband().getIndividual().getEventsOfType(IndividualEventType.BIRTH).get(0) != null){
+                    husbandbirth = family.getHusband().getIndividual().getEventsOfType(IndividualEventType.BIRTH).get(0).getDate().toString();
+                }
+                if (family.getWife().getIndividual().getEventsOfType(IndividualEventType.BIRTH).size() > 0 && family.getWife().getIndividual().getEventsOfType(IndividualEventType.BIRTH).get(0) != null){
+                    wifebirth = family.getWife().getIndividual().getEventsOfType(IndividualEventType.BIRTH).get(0).getDate().toString();
+                }
+                if(!marriage.equals("") && !marriage.equals(null)) {
+                    int marriagedate = Integer.parseInt(marriage.substring(marriage.length() - 4));
+                    if(!husbandbirth.equals("") && !husbandbirth.equals(null)){
+                        int husbanddate = Integer.parseInt(husbandbirth.substring(husbandbirth.length() - 4));
+                        if(husbanddate > marriagedate){
+                            return false;
+                        }
+                    }
+                    if(!wifebirth.equals("") && !wifebirth.equals(null)){
+                        int wifedate = Integer.parseInt(wifebirth.substring(wifebirth.length() - 4));
+                        if(wifedate > marriagedate){
+                            return false;
+                        }
+                    }
+                }
             }
         }
-
-        ArrayList<Integer> birth = new ArrayList();
-        birth.add(Integer.parseInt(Birth.get(0).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(1).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(2).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(3).toString().substring(6,10)));
-        birth.add(Integer.parseInt(Birth.get(4).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(5).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(6).toString().substring(7,11)));
-        birth.add(Integer.parseInt(Birth.get(7).toString().substring(7,11)));
-        birth.add(0);
-        birth.add(0);
-        birth.add(Integer.parseInt(Birth.get(10).toString().substring(0,4)));
-        birth.add(Integer.parseInt(Birth.get(11).toString().substring(4,8)));
-        birth.add(Integer.parseInt(Birth.get(12).toString().substring(0,4)));
-        birth.add(Integer.parseInt(Birth.get(13).toString().substring(0,4)));
-
-        for(int i = 0; i < fam.length; i++){
-            if(birth.get(fam[i][0] - 1) > fam[i][2]){
-                invalid.add(Integer.toString(birth.get(fam[i][0])));
-            }
-            if(birth.get(fam[i][1] - 1) > fam[i][2]){
-                invalid.add(Integer.toString(birth.get(fam[i][1])));
-            }
-        }
-
-        return invalid;
+        return true;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list = birthMarri("GEDCOMsourcefile/bronte.ged");
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
+    public static void main(String[] args) throws IOException, GedcomParserException {
+        birthMarri("src/resources/GEDCOMsourcefile/bronte.ged");
     }
 }
--- src/test/java/org/Gedcom/BirthBeforeMarriageTest.java
+++ src/test/java/org/Gedcom/BirthBeforeMarriageTest.java
@@ -2,45 +2,36 @@ package org.Gedcom;
 
 import junit.framework.TestCase;
 import org.Gedcom.BirthBeforeMarriage;
+import org.gedcom4j.exception.GedcomParserException;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.Gedcom.BirthBeforeMarriage.birthMarri;
+
 public class BirthBeforeMarriageTest extends TestCase {
-    BirthBeforeMarriage bbm = new BirthBeforeMarriage();
-    public void test1() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list.add("1783");
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte1.ged"),list);
+    public void test1() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte1.ged"),false);
     }
 
-    public void test2() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list.add("1814");
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte2.ged"),list);
+    public void test2() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte2.ged"),true);
     }
 
-    public void test3() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list.add("1817");
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte3.ged"),list);
+    public void test3() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte3.ged"),false);
     }
 
-    public void test4() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list.add("1744");
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte4.ged"),list);
+    public void test4() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte4.ged"),false);
     }
 
-    public void test5() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        list.add("1783");
-        list.add("1776");
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte5.ged"),list);
+    public void test5() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/EditedFamilyTree/bronte5.ged"),false);
     }
 
-    public void test6() throws FileNotFoundException {
-        ArrayList list = new ArrayList();
-        assertEquals(bbm.birthMarri("src/resources/GEDCOMsourcefile/bronte.ged"),list);
+    public void test6() throws IOException, GedcomParserException {
+        assertEquals(birthMarri("src/resources/GEDCOMsourcefile/bronte.ged"),true);
     }
 }
